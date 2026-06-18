fc802186a69410656d6370d3544f914e8cf2fb9e
Maksim Leskin <quper2405@gmail.com>
2026-06-18 15:17:07 +0300

    upd 1.59

--- _03_script_price_upd.js
+++ _03_script_price_upd.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
 
-const FIX_COEF = 100 / 84.39;
+const FIX_COEF = 100 / 84.81;
 
 // Путь к папке с файлами стран
 const countryDir = path.join(__dirname, "def", "country");
